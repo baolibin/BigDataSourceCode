730064216f33ae001f21017daa634d028f8ec775
baolibin <2578858653@qq.com>
2021-01-10 17:12:18 +0800

    BlockId

--- src/main/scala/org/apache/spark/storage/BlockId.scala
+++ src/main/scala/org/apache/spark/storage/BlockId.scala
@@ -22,6 +22,7 @@ import java.util.UUID
 import org.apache.spark.annotation.DeveloperApi
 
 /**
+ * 数据标识为一个特别的Block块，通常与一个单独的文件相关联。
  * :: DeveloperApi ::
  * Identifies a particular Block of data, usually associated with a single file.
  * A Block can be uniquely identified by its filename, but each type of Block has a different
@@ -31,105 +32,113 @@ import org.apache.spark.annotation.DeveloperApi
  */
 @DeveloperApi
 sealed abstract class BlockId {
-  /** A globally unique identifier for this Block. Can be used for ser/de. */
-  def name: String
+    /**
+     * Block块全局唯一标识，序列化/反序列化都可以使用。
+     * A globally unique identifier for this Block. Can be used for ser/de.
+     */
+    def name: String
 
-  // convenience methods
-  def asRDDId: Option[RDDBlockId] = if (isRDD) Some(asInstanceOf[RDDBlockId]) else None
-  def isRDD: Boolean = isInstanceOf[RDDBlockId]
-  def isShuffle: Boolean = isInstanceOf[ShuffleBlockId]
-  def isBroadcast: Boolean = isInstanceOf[BroadcastBlockId]
+    // convenience methods
+    def asRDDId: Option[RDDBlockId] = if (isRDD) Some(asInstanceOf[RDDBlockId]) else None
 
-  override def toString: String = name
-  override def hashCode: Int = name.hashCode
-  override def equals(other: Any): Boolean = other match {
-    case o: BlockId => getClass == o.getClass && name.equals(o.name)
-    case _ => false
-  }
+    def isRDD: Boolean = isInstanceOf[RDDBlockId]
+
+    def isShuffle: Boolean = isInstanceOf[ShuffleBlockId]
+
+    def isBroadcast: Boolean = isInstanceOf[BroadcastBlockId]
+
+    override def toString: String = name
+
+    override def hashCode: Int = name.hashCode
+
+    override def equals(other: Any): Boolean = other match {
+        case o: BlockId => getClass == o.getClass && name.equals(o.name)
+        case _ => false
+    }
 }
 
 @DeveloperApi
 case class RDDBlockId(rddId: Int, splitIndex: Int) extends BlockId {
-  override def name: String = "rdd_" + rddId + "_" + splitIndex
+    override def name: String = "rdd_" + rddId + "_" + splitIndex
 }
 
 // Format of the shuffle block ids (including data and index) should be kept in sync with
 // org.apache.spark.network.shuffle.ExternalShuffleBlockResolver#getBlockData().
 @DeveloperApi
 case class ShuffleBlockId(shuffleId: Int, mapId: Int, reduceId: Int) extends BlockId {
-  override def name: String = "shuffle_" + shuffleId + "_" + mapId + "_" + reduceId
+    override def name: String = "shuffle_" + shuffleId + "_" + mapId + "_" + reduceId
 }
 
 @DeveloperApi
 case class ShuffleDataBlockId(shuffleId: Int, mapId: Int, reduceId: Int) extends BlockId {
-  override def name: String = "shuffle_" + shuffleId + "_" + mapId + "_" + reduceId + ".data"
+    override def name: String = "shuffle_" + shuffleId + "_" + mapId + "_" + reduceId + ".data"
 }
 
 @DeveloperApi
 case class ShuffleIndexBlockId(shuffleId: Int, mapId: Int, reduceId: Int) extends BlockId {
-  override def name: String = "shuffle_" + shuffleId + "_" + mapId + "_" + reduceId + ".index"
+    override def name: String = "shuffle_" + shuffleId + "_" + mapId + "_" + reduceId + ".index"
 }
 
 @DeveloperApi
 case class BroadcastBlockId(broadcastId: Long, field: String = "") extends BlockId {
-  override def name: String = "broadcast_" + broadcastId + (if (field == "") "" else "_" + field)
+    override def name: String = "broadcast_" + broadcastId + (if (field == "") "" else "_" + field)
 }
 
 @DeveloperApi
 case class TaskResultBlockId(taskId: Long) extends BlockId {
-  override def name: String = "taskresult_" + taskId
+    override def name: String = "taskresult_" + taskId
 }
 
 @DeveloperApi
 case class StreamBlockId(streamId: Int, uniqueId: Long) extends BlockId {
-  override def name: String = "input-" + streamId + "-" + uniqueId
+    override def name: String = "input-" + streamId + "-" + uniqueId
 }
 
 /** Id associated with temporary local data managed as blocks. Not serializable. */
 private[spark] case class TempLocalBlockId(id: UUID) extends BlockId {
-  override def name: String = "temp_local_" + id
+    override def name: String = "temp_local_" + id
 }
 
 /** Id associated with temporary shuffle data managed as blocks. Not serializable. */
 private[spark] case class TempShuffleBlockId(id: UUID) extends BlockId {
-  override def name: String = "temp_shuffle_" + id
+    override def name: String = "temp_shuffle_" + id
 }
 
 // Intended only for testing purposes
 private[spark] case class TestBlockId(id: String) extends BlockId {
-  override def name: String = "test_" + id
+    override def name: String = "test_" + id
 }
 
 @DeveloperApi
 object BlockId {
-  val RDD = "rdd_([0-9]+)_([0-9]+)".r
-  val SHUFFLE = "shuffle_([0-9]+)_([0-9]+)_([0-9]+)".r
-  val SHUFFLE_DATA = "shuffle_([0-9]+)_([0-9]+)_([0-9]+).data".r
-  val SHUFFLE_INDEX = "shuffle_([0-9]+)_([0-9]+)_([0-9]+).index".r
-  val BROADCAST = "broadcast_([0-9]+)([_A-Za-z0-9]*)".r
-  val TASKRESULT = "taskresult_([0-9]+)".r
-  val STREAM = "input-([0-9]+)-([0-9]+)".r
-  val TEST = "test_(.*)".r
+    val RDD = "rdd_([0-9]+)_([0-9]+)".r
+    val SHUFFLE = "shuffle_([0-9]+)_([0-9]+)_([0-9]+)".r
+    val SHUFFLE_DATA = "shuffle_([0-9]+)_([0-9]+)_([0-9]+).data".r
+    val SHUFFLE_INDEX = "shuffle_([0-9]+)_([0-9]+)_([0-9]+).index".r
+    val BROADCAST = "broadcast_([0-9]+)([_A-Za-z0-9]*)".r
+    val TASKRESULT = "taskresult_([0-9]+)".r
+    val STREAM = "input-([0-9]+)-([0-9]+)".r
+    val TEST = "test_(.*)".r
 
-  /** Converts a BlockId "name" String back into a BlockId. */
-  def apply(id: String): BlockId = id match {
-    case RDD(rddId, splitIndex) =>
-      RDDBlockId(rddId.toInt, splitIndex.toInt)
-    case SHUFFLE(shuffleId, mapId, reduceId) =>
-      ShuffleBlockId(shuffleId.toInt, mapId.toInt, reduceId.toInt)
-    case SHUFFLE_DATA(shuffleId, mapId, reduceId) =>
-      ShuffleDataBlockId(shuffleId.toInt, mapId.toInt, reduceId.toInt)
-    case SHUFFLE_INDEX(shuffleId, mapId, reduceId) =>
-      ShuffleIndexBlockId(shuffleId.toInt, mapId.toInt, reduceId.toInt)
-    case BROADCAST(broadcastId, field) =>
-      BroadcastBlockId(broadcastId.toLong, field.stripPrefix("_"))
-    case TASKRESULT(taskId) =>
-      TaskResultBlockId(taskId.toLong)
-    case STREAM(streamId, uniqueId) =>
-      StreamBlockId(streamId.toInt, uniqueId.toLong)
-    case TEST(value) =>
-      TestBlockId(value)
-    case _ =>
-      throw new IllegalStateException("Unrecognized BlockId: " + id)
-  }
+    /** Converts a BlockId "name" String back into a BlockId. */
+    def apply(id: String): BlockId = id match {
+        case RDD(rddId, splitIndex) =>
+            RDDBlockId(rddId.toInt, splitIndex.toInt)
+        case SHUFFLE(shuffleId, mapId, reduceId) =>
+            ShuffleBlockId(shuffleId.toInt, mapId.toInt, reduceId.toInt)
+        case SHUFFLE_DATA(shuffleId, mapId, reduceId) =>
+            ShuffleDataBlockId(shuffleId.toInt, mapId.toInt, reduceId.toInt)
+        case SHUFFLE_INDEX(shuffleId, mapId, reduceId) =>
+            ShuffleIndexBlockId(shuffleId.toInt, mapId.toInt, reduceId.toInt)
+        case BROADCAST(broadcastId, field) =>
+            BroadcastBlockId(broadcastId.toLong, field.stripPrefix("_"))
+        case TASKRESULT(taskId) =>
+            TaskResultBlockId(taskId.toLong)
+        case STREAM(streamId, uniqueId) =>
+            StreamBlockId(streamId.toInt, uniqueId.toLong)
+        case TEST(value) =>
+            TestBlockId(value)
+        case _ =>
+            throw new IllegalStateException("Unrecognized BlockId: " + id)
+    }
 }
