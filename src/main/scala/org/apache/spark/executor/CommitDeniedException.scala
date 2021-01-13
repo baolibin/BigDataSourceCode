a4a0da8905541384ebbf1103782c41e3e152be13
baolibin <2578858653@qq.com>
2021-01-13 23:19:32 +0800

    CommitDeniedException

--- src/main/scala/org/apache/spark/executor/CommitDeniedException.scala
+++ src/main/scala/org/apache/spark/executor/CommitDeniedException.scala
@@ -20,14 +20,15 @@ package org.apache.spark.executor
 import org.apache.spark.{TaskCommitDenied, TaskFailedReason}
 
 /**
- * Exception thrown when a task attempts to commit output to HDFS but is denied by the driver.
- */
+  * task试图向HDFS输出内容，但被driver拒绝异常。
+  * Exception thrown when a task attempts to commit output to HDFS but is denied by the driver.
+  */
 private[spark] class CommitDeniedException(
-    msg: String,
-    jobID: Int,
-    splitID: Int,
-    attemptNumber: Int)
-  extends Exception(msg) {
+                                                  msg: String,
+                                                  jobID: Int,
+                                                  splitID: Int,
+                                                  attemptNumber: Int)
+        extends Exception(msg) {
 
-  def toTaskFailedReason: TaskFailedReason = TaskCommitDenied(jobID, splitID, attemptNumber)
+    def toTaskFailedReason: TaskFailedReason = TaskCommitDenied(jobID, splitID, attemptNumber)
 }
