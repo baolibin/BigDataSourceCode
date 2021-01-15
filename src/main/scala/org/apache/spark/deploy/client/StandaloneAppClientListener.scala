76eaa3ce5bd8c89219efa42ada000ea6b4904def
baolibin <2578858653@qq.com>
2021-01-15 22:46:33 +0800

    StandaloneAppClient

--- src/main/scala/org/apache/spark/deploy/client/StandaloneAppClientListener.scala
+++ src/main/scala/org/apache/spark/deploy/client/StandaloneAppClientListener.scala
@@ -18,24 +18,26 @@
 package org.apache.spark.deploy.client
 
 /**
- * Callbacks invoked by deploy client when various events happen. There are currently four events:
- * connecting to the cluster, disconnecting, being given an executor, and having an executor
- * removed (either due to failure or due to revocation).
- *
- * Users of this API should *not* block inside the callback methods.
- */
+  * 部署客户端时，发生各种消息的回调函数。
+  *
+  * Callbacks invoked by deploy client when various events happen. There are currently four events:
+  * connecting to the cluster, disconnecting, being given an executor, and having an executor
+  * removed (either due to failure or due to revocation).
+  *
+  * Users of this API should *not* block inside the callback methods.
+  */
 private[spark] trait StandaloneAppClientListener {
-  def connected(appId: String): Unit
+    def connected(appId: String): Unit
 
-  /** Disconnection may be a temporary state, as we fail over to a new Master. */
-  def disconnected(): Unit
+    /** Disconnection may be a temporary state, as we fail over to a new Master. */
+    def disconnected(): Unit
 
-  /** An application death is an unrecoverable failure condition. */
-  def dead(reason: String): Unit
+    /** An application death is an unrecoverable failure condition. */
+    def dead(reason: String): Unit
 
-  def executorAdded(
-      fullId: String, workerId: String, hostPort: String, cores: Int, memory: Int): Unit
+    def executorAdded(
+                             fullId: String, workerId: String, hostPort: String, cores: Int, memory: Int): Unit
 
-  def executorRemoved(
-      fullId: String, message: String, exitStatus: Option[Int], workerLost: Boolean): Unit
+    def executorRemoved(
+                               fullId: String, message: String, exitStatus: Option[Int], workerLost: Boolean): Unit
 }
