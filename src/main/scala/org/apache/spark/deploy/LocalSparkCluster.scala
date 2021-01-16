52c920068394ecfcc650b6bd34c2e77ab19a1756
baolibin <2578858653@qq.com>
2021-01-16 22:44:58 +0800

    LocalSparkCluster

--- src/main/scala/org/apache/spark/deploy/LocalSparkCluster.scala
+++ src/main/scala/org/apache/spark/deploy/LocalSparkCluster.scala
@@ -17,8 +17,6 @@
 
 package org.apache.spark.deploy
 
-import scala.collection.mutable.ArrayBuffer
-
 import org.apache.spark.SparkConf
 import org.apache.spark.deploy.master.Master
 import org.apache.spark.deploy.worker.Worker
@@ -26,59 +24,63 @@ import org.apache.spark.internal.Logging
 import org.apache.spark.rpc.RpcEnv
 import org.apache.spark.util.Utils
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
- * Testing class that creates a Spark standalone process in-cluster (that is, running the
- * spark.deploy.master.Master and spark.deploy.worker.Workers in the same JVMs). Executors launched
- * by the Workers still run in separate JVMs. This can be used to test distributed operation and
- * fault recovery without spinning up a lot of processes.
- */
+  * 在集群中创建Spark独立进程的测试类。
+  *
+  * Testing class that creates a Spark standalone process in-cluster (that is, running the
+  * spark.deploy.master.Master and spark.deploy.worker.Workers in the same JVMs). Executors launched
+  * by the Workers still run in separate JVMs. This can be used to test distributed operation and
+  * fault recovery without spinning up a lot of processes.
+  */
 private[spark]
 class LocalSparkCluster(
-    numWorkers: Int,
-    coresPerWorker: Int,
-    memoryPerWorker: Int,
-    conf: SparkConf)
-  extends Logging {
+                               numWorkers: Int,
+                               coresPerWorker: Int,
+                               memoryPerWorker: Int,
+                               conf: SparkConf)
+        extends Logging {
 
-  private val localHostname = Utils.localHostName()
-  private val masterRpcEnvs = ArrayBuffer[RpcEnv]()
-  private val workerRpcEnvs = ArrayBuffer[RpcEnv]()
-  // exposed for testing
-  var masterWebUIPort = -1
+    private val localHostname = Utils.localHostName()
+    private val masterRpcEnvs = ArrayBuffer[RpcEnv]()
+    private val workerRpcEnvs = ArrayBuffer[RpcEnv]()
+    // exposed for testing
+    var masterWebUIPort = -1
 
-  def start(): Array[String] = {
-    logInfo("Starting a local Spark cluster with " + numWorkers + " workers.")
+    def start(): Array[String] = {
+        logInfo("Starting a local Spark cluster with " + numWorkers + " workers.")
 
-    // Disable REST server on Master in this mode unless otherwise specified
-    val _conf = conf.clone()
-      .setIfMissing("spark.master.rest.enabled", "false")
-      .set("spark.shuffle.service.enabled", "false")
+        // Disable REST server on Master in this mode unless otherwise specified
+        val _conf = conf.clone()
+                .setIfMissing("spark.master.rest.enabled", "false")
+                .set("spark.shuffle.service.enabled", "false")
 
-    /* Start the Master */
-    val (rpcEnv, webUiPort, _) = Master.startRpcEnvAndEndpoint(localHostname, 0, 0, _conf)
-    masterWebUIPort = webUiPort
-    masterRpcEnvs += rpcEnv
-    val masterUrl = "spark://" + Utils.localHostNameForURI() + ":" + rpcEnv.address.port
-    val masters = Array(masterUrl)
+        /* Start the Master */
+        val (rpcEnv, webUiPort, _) = Master.startRpcEnvAndEndpoint(localHostname, 0, 0, _conf)
+        masterWebUIPort = webUiPort
+        masterRpcEnvs += rpcEnv
+        val masterUrl = "spark://" + Utils.localHostNameForURI() + ":" + rpcEnv.address.port
+        val masters = Array(masterUrl)
 
-    /* Start the Workers */
-    for (workerNum <- 1 to numWorkers) {
-      val workerEnv = Worker.startRpcEnvAndEndpoint(localHostname, 0, 0, coresPerWorker,
-        memoryPerWorker, masters, null, Some(workerNum), _conf)
-      workerRpcEnvs += workerEnv
+        /* Start the Workers */
+        for (workerNum <- 1 to numWorkers) {
+            val workerEnv = Worker.startRpcEnvAndEndpoint(localHostname, 0, 0, coresPerWorker,
+                memoryPerWorker, masters, null, Some(workerNum), _conf)
+            workerRpcEnvs += workerEnv
+        }
+
+        masters
     }
 
-    masters
-  }
-
-  def stop() {
-    logInfo("Shutting down local Spark cluster.")
-    // Stop the workers before the master so they don't get upset that it disconnected
-    workerRpcEnvs.foreach(_.shutdown())
-    masterRpcEnvs.foreach(_.shutdown())
-    workerRpcEnvs.foreach(_.awaitTermination())
-    masterRpcEnvs.foreach(_.awaitTermination())
-    masterRpcEnvs.clear()
-    workerRpcEnvs.clear()
-  }
+    def stop() {
+        logInfo("Shutting down local Spark cluster.")
+        // Stop the workers before the master so they don't get upset that it disconnected
+        workerRpcEnvs.foreach(_.shutdown())
+        masterRpcEnvs.foreach(_.shutdown())
+        workerRpcEnvs.foreach(_.awaitTermination())
+        masterRpcEnvs.foreach(_.awaitTermination())
+        masterRpcEnvs.clear()
+        workerRpcEnvs.clear()
+    }
 }
--- src/main/scala/org/apache/spark/deploy/PythonRunner.scala
+++ src/main/scala/org/apache/spark/deploy/PythonRunner.scala
@@ -20,129 +20,131 @@ package org.apache.spark.deploy
 import java.io.File
 import java.net.URI
 
-import scala.collection.mutable.ArrayBuffer
-import scala.collection.JavaConverters._
-import scala.util.Try
-
-import org.apache.spark.{SparkConf, SparkUserAppException}
 import org.apache.spark.api.python.PythonUtils
 import org.apache.spark.internal.config._
 import org.apache.spark.util.{RedirectThread, Utils}
+import org.apache.spark.{SparkConf, SparkUserAppException}
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Try
 
 /**
- * A main class used to launch Python applications. It executes python as a
- * subprocess and then has it connect back to the JVM to access system properties, etc.
- */
+  * 用于启动Python应用程序的主类。它将python作为子进程执行，然后将其连接回JVM以访问系统属性等。
+  *
+  * A main class used to launch Python applications. It executes python as a
+  * subprocess and then has it connect back to the JVM to access system properties, etc.
+  */
 object PythonRunner {
-  def main(args: Array[String]) {
-    val pythonFile = args(0)
-    val pyFiles = args(1)
-    val otherArgs = args.slice(2, args.length)
-    val sparkConf = new SparkConf()
-    val pythonExec = sparkConf.get(PYSPARK_DRIVER_PYTHON)
-      .orElse(sparkConf.get(PYSPARK_PYTHON))
-      .orElse(sys.env.get("PYSPARK_DRIVER_PYTHON"))
-      .orElse(sys.env.get("PYSPARK_PYTHON"))
-      .getOrElse("python")
+    def main(args: Array[String]) {
+        val pythonFile = args(0)
+        val pyFiles = args(1)
+        val otherArgs = args.slice(2, args.length)
+        val sparkConf = new SparkConf()
+        val pythonExec = sparkConf.get(PYSPARK_DRIVER_PYTHON)
+                .orElse(sparkConf.get(PYSPARK_PYTHON))
+                .orElse(sys.env.get("PYSPARK_DRIVER_PYTHON"))
+                .orElse(sys.env.get("PYSPARK_PYTHON"))
+                .getOrElse("python")
 
-    // Format python file paths before adding them to the PYTHONPATH
-    val formattedPythonFile = formatPath(pythonFile)
-    val formattedPyFiles = formatPaths(pyFiles)
+        // Format python file paths before adding them to the PYTHONPATH
+        val formattedPythonFile = formatPath(pythonFile)
+        val formattedPyFiles = formatPaths(pyFiles)
 
-    // Launch a Py4J gateway server for the process to connect to; this will let it see our
-    // Java system properties and such
-    val gatewayServer = new py4j.GatewayServer(null, 0)
-    val thread = new Thread(new Runnable() {
-      override def run(): Unit = Utils.logUncaughtExceptions {
-        gatewayServer.start()
-      }
-    })
-    thread.setName("py4j-gateway-init")
-    thread.setDaemon(true)
-    thread.start()
+        // Launch a Py4J gateway server for the process to connect to; this will let it see our
+        // Java system properties and such
+        val gatewayServer = new py4j.GatewayServer(null, 0)
+        val thread = new Thread(new Runnable() {
+            override def run(): Unit = Utils.logUncaughtExceptions {
+                gatewayServer.start()
+            }
+        })
+        thread.setName("py4j-gateway-init")
+        thread.setDaemon(true)
+        thread.start()
 
-    // Wait until the gateway server has started, so that we know which port is it bound to.
-    // `gatewayServer.start()` will start a new thread and run the server code there, after
-    // initializing the socket, so the thread started above will end as soon as the server is
-    // ready to serve connections.
-    thread.join()
+        // Wait until the gateway server has started, so that we know which port is it bound to.
+        // `gatewayServer.start()` will start a new thread and run the server code there, after
+        // initializing the socket, so the thread started above will end as soon as the server is
+        // ready to serve connections.
+        thread.join()
 
-    // Build up a PYTHONPATH that includes the Spark assembly (where this class is), the
-    // python directories in SPARK_HOME (if set), and any files in the pyFiles argument
-    val pathElements = new ArrayBuffer[String]
-    pathElements ++= formattedPyFiles
-    pathElements += PythonUtils.sparkPythonPath
-    pathElements += sys.env.getOrElse("PYTHONPATH", "")
-    val pythonPath = PythonUtils.mergePythonPaths(pathElements: _*)
+        // Build up a PYTHONPATH that includes the Spark assembly (where this class is), the
+        // python directories in SPARK_HOME (if set), and any files in the pyFiles argument
+        val pathElements = new ArrayBuffer[String]
+        pathElements ++= formattedPyFiles
+        pathElements += PythonUtils.sparkPythonPath
+        pathElements += sys.env.getOrElse("PYTHONPATH", "")
+        val pythonPath = PythonUtils.mergePythonPaths(pathElements: _*)
 
-    // Launch Python process
-    val builder = new ProcessBuilder((Seq(pythonExec, formattedPythonFile) ++ otherArgs).asJava)
-    val env = builder.environment()
-    env.put("PYTHONPATH", pythonPath)
-    // This is equivalent to setting the -u flag; we use it because ipython doesn't support -u:
-    env.put("PYTHONUNBUFFERED", "YES") // value is needed to be set to a non-empty string
-    env.put("PYSPARK_GATEWAY_PORT", "" + gatewayServer.getListeningPort)
-    // pass conf spark.pyspark.python to python process, the only way to pass info to
-    // python process is through environment variable.
-    sparkConf.get(PYSPARK_PYTHON).foreach(env.put("PYSPARK_PYTHON", _))
-    sys.env.get("PYTHONHASHSEED").foreach(env.put("PYTHONHASHSEED", _))
-    builder.redirectErrorStream(true) // Ugly but needed for stdout and stderr to synchronize
-    try {
-      val process = builder.start()
+        // Launch Python process
+        val builder = new ProcessBuilder((Seq(pythonExec, formattedPythonFile) ++ otherArgs).asJava)
+        val env = builder.environment()
+        env.put("PYTHONPATH", pythonPath)
+        // This is equivalent to setting the -u flag; we use it because ipython doesn't support -u:
+        env.put("PYTHONUNBUFFERED", "YES") // value is needed to be set to a non-empty string
+        env.put("PYSPARK_GATEWAY_PORT", "" + gatewayServer.getListeningPort)
+        // pass conf spark.pyspark.python to python process, the only way to pass info to
+        // python process is through environment variable.
+        sparkConf.get(PYSPARK_PYTHON).foreach(env.put("PYSPARK_PYTHON", _))
+        sys.env.get("PYTHONHASHSEED").foreach(env.put("PYTHONHASHSEED", _))
+        builder.redirectErrorStream(true) // Ugly but needed for stdout and stderr to synchronize
+        try {
+            val process = builder.start()
 
-      new RedirectThread(process.getInputStream, System.out, "redirect output").start()
+            new RedirectThread(process.getInputStream, System.out, "redirect output").start()
 
-      val exitCode = process.waitFor()
-      if (exitCode != 0) {
-        throw new SparkUserAppException(exitCode)
-      }
-    } finally {
-      gatewayServer.shutdown()
-    }
-  }
-
-  /**
-   * Format the python file path so that it can be added to the PYTHONPATH correctly.
-   *
-   * Python does not understand URI schemes in paths. Before adding python files to the
-   * PYTHONPATH, we need to extract the path from the URI. This is safe to do because we
-   * currently only support local python files.
-   */
-  def formatPath(path: String, testWindows: Boolean = false): String = {
-    if (Utils.nonLocalPaths(path, testWindows).nonEmpty) {
-      throw new IllegalArgumentException("Launching Python applications through " +
-        s"spark-submit is currently only supported for local files: $path")
-    }
-    // get path when scheme is file.
-    val uri = Try(new URI(path)).getOrElse(new File(path).toURI)
-    var formattedPath = uri.getScheme match {
-      case null => path
-      case "file" | "local" => uri.getPath
-      case _ => null
+            val exitCode = process.waitFor()
+            if (exitCode != 0) {
+                throw new SparkUserAppException(exitCode)
+            }
+        } finally {
+            gatewayServer.shutdown()
+        }
     }
 
-    // Guard against malformed paths potentially throwing NPE
-    if (formattedPath == null) {
-      throw new IllegalArgumentException(s"Python file path is malformed: $path")
+    /**
+      * Format each python file path in the comma-delimited list of paths, so it can be
+      * added to the PYTHONPATH correctly.
+      */
+    def formatPaths(paths: String, testWindows: Boolean = false): Array[String] = {
+        Option(paths).getOrElse("")
+                .split(",")
+                .filter(_.nonEmpty)
+                .map { p => formatPath(p, testWindows) }
     }
 
-    // In Windows, the drive should not be prefixed with "/"
-    // For instance, python does not understand "/C:/path/to/sheep.py"
-    if (Utils.isWindows && formattedPath.matches("/[a-zA-Z]:/.*")) {
-      formattedPath = formattedPath.stripPrefix("/")
-    }
-    formattedPath
-  }
+    /**
+      * Format the python file path so that it can be added to the PYTHONPATH correctly.
+      *
+      * Python does not understand URI schemes in paths. Before adding python files to the
+      * PYTHONPATH, we need to extract the path from the URI. This is safe to do because we
+      * currently only support local python files.
+      */
+    def formatPath(path: String, testWindows: Boolean = false): String = {
+        if (Utils.nonLocalPaths(path, testWindows).nonEmpty) {
+            throw new IllegalArgumentException("Launching Python applications through " +
+                    s"spark-submit is currently only supported for local files: $path")
+        }
+        // get path when scheme is file.
+        val uri = Try(new URI(path)).getOrElse(new File(path).toURI)
+        var formattedPath = uri.getScheme match {
+            case null => path
+            case "file" | "local" => uri.getPath
+            case _ => null
+        }
 
-  /**
-   * Format each python file path in the comma-delimited list of paths, so it can be
-   * added to the PYTHONPATH correctly.
-   */
-  def formatPaths(paths: String, testWindows: Boolean = false): Array[String] = {
-    Option(paths).getOrElse("")
-      .split(",")
-      .filter(_.nonEmpty)
-      .map { p => formatPath(p, testWindows) }
-  }
+        // Guard against malformed paths potentially throwing NPE
+        if (formattedPath == null) {
+            throw new IllegalArgumentException(s"Python file path is malformed: $path")
+        }
+
+        // In Windows, the drive should not be prefixed with "/"
+        // For instance, python does not understand "/C:/path/to/sheep.py"
+        if (Utils.isWindows && formattedPath.matches("/[a-zA-Z]:/.*")) {
+            formattedPath = formattedPath.stripPrefix("/")
+        }
+        formattedPath
+    }
 
 }
