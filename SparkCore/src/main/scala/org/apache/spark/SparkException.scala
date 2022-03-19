86b13cd1ea037483ec8d8ba2a9c33914c76170cd
baolibin <2578858653@qq.com>
2022-03-19 19:52:47 +0800

    update

--- SparkCore/src/main/scala/org/apache/spark/SparkException.scala
+++ SparkCore/src/main/scala/org/apache/spark/SparkException.scala
@@ -18,22 +18,26 @@
 package org.apache.spark
 
 class SparkException(message: String, cause: Throwable)
-  extends Exception(message, cause) {
+        extends Exception(message, cause) {
 
-  def this(message: String) = this(message, null)
+    def this(message: String) = this(message, null)
 }
 
 /**
- * Exception thrown when execution of some user code in the driver process fails, e.g.
- * accumulator update fails or failure in takeOrdered (user supplies an Ordering implementation
- * that can be misbehaving.
- */
+  * 当驱动程序进程中的某些用户代码执行失败时引发异常，
+  * 例如累加器更新失败或takeOrdered中的失败（用户提供的排序实现可能会出现错误）。
+  * Exception thrown when execution of some user code in the driver process fails, e.g.
+  * accumulator update fails or failure in takeOrdered (user supplies an Ordering implementation
+  * that can be misbehaving.
+  */
 private[spark] class SparkDriverExecutionException(cause: Throwable)
-  extends SparkException("Execution error", cause)
+        extends SparkException("Execution error", cause)
 
 /**
- * Exception thrown when the main user code is run as a child process (e.g. pyspark) and we want
- * the parent SparkSubmit process to exit with the same exit code.
- */
+  * 当主用户代码作为子进程（例如pyspark）运行，并且我们希望父SparkSubmit进程使用相同的退出代码退出时引发异常。
+  *
+  * Exception thrown when the main user code is run as a child process (e.g. pyspark) and we want
+  * the parent SparkSubmit process to exit with the same exit code.
+  */
 private[spark] case class SparkUserAppException(exitCode: Int)
-  extends SparkException(s"User application exited with $exitCode")
+        extends SparkException(s"User application exited with $exitCode")
