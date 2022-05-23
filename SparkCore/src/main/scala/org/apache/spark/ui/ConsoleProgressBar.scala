b740ae928a3696df10e2cf51dd1d5ac203906673
baolibin <2578858653@qq.com>
2022-05-23 21:45:02 +0800

    update

--- SparkCore/src/main/scala/org/apache/spark/ui/ConsoleProgressBar.scala
+++ SparkCore/src/main/scala/org/apache/spark/ui/ConsoleProgressBar.scala
@@ -23,109 +23,113 @@ import org.apache.spark._
 import org.apache.spark.internal.Logging
 
 /**
- * ConsoleProgressBar shows the progress of stages in the next line of the console. It poll the
- * status of active stages from `sc.statusTracker` periodically, the progress bar will be showed
- * up after the stage has ran at least 500ms. If multiple stages run in the same time, the status
- * of them will be combined together, showed in one line.
- */
+  * ConsoleProgressBar shows the progress of stages in the next line of the console. It poll the
+  * status of active stages from `sc.statusTracker` periodically, the progress bar will be showed
+  * up after the stage has ran at least 500ms. If multiple stages run in the same time, the status
+  * of them will be combined together, showed in one line.
+  */
 private[spark] class ConsoleProgressBar(sc: SparkContext) extends Logging {
-  // Carriage return
-  private val CR = '\r'
-  // Update period of progress bar, in milliseconds
-  private val updatePeriodMSec =
-    sc.getConf.getTimeAsMs("org.apache.spark.ui.consoleProgress.update.interval", "200")
-  // Delay to show up a progress bar, in milliseconds
-  private val firstDelayMSec = 500L
+    // Carriage return
+    private val CR = '\r'
+    // Update period of progress bar, in milliseconds
+    private val updatePeriodMSec =
+        sc.getConf.getTimeAsMs("org.apache.spark.ui.consoleProgress.update.interval", "200")
+    // Delay to show up a progress bar, in milliseconds
+    private val firstDelayMSec = 500L
 
-  // The width of terminal
-  private val TerminalWidth = if (!sys.env.getOrElse("COLUMNS", "").isEmpty) {
-    sys.env.get("COLUMNS").get.toInt
-  } else {
-    80
-  }
-
-  private var lastFinishTime = 0L
-  private var lastUpdateTime = 0L
-  private var lastProgressBar = ""
-
-  // Schedule a refresh thread to run periodically
-  private val timer = new Timer("refresh progress", true)
-  timer.schedule(new TimerTask{
-    override def run() {
-      refresh()
+    // The width of terminal
+    private val TerminalWidth = if (!sys.env.getOrElse("COLUMNS", "").isEmpty) {
+        sys.env.get("COLUMNS").get.toInt
+    } else {
+        80
     }
-  }, firstDelayMSec, updatePeriodMSec)
 
-  /**
-   * Try to refresh the progress bar in every cycle
-   */
-  private def refresh(): Unit = synchronized {
-    val now = System.currentTimeMillis()
-    if (now - lastFinishTime < firstDelayMSec) {
-      return
-    }
-    val stageIds = sc.statusTracker.getActiveStageIds()
-    val stages = stageIds.flatMap(sc.statusTracker.getStageInfo).filter(_.numTasks() > 1)
-      .filter(now - _.submissionTime() > firstDelayMSec).sortBy(_.stageId())
-    if (stages.length > 0) {
-      show(now, stages.take(3))  // display at most 3 stages in same time
-    }
-  }
+    private var lastFinishTime = 0L
+    private var lastUpdateTime = 0L
+    private var lastProgressBar = ""
 
-  /**
-   * Show progress bar in console. The progress bar is displayed in the next line
-   * after your last output, keeps overwriting itself to hold in one line. The logging will follow
-   * the progress bar, then progress bar will be showed in next line without overwrite logs.
-   */
-  private def show(now: Long, stages: Seq[SparkStageInfo]) {
-    val width = TerminalWidth / stages.size
-    val bar = stages.map { s =>
-      val total = s.numTasks()
-      val header = s"[Stage ${s.stageId()}:"
-      val tailer = s"(${s.numCompletedTasks()} + ${s.numActiveTasks()}) / $total]"
-      val w = width - header.length - tailer.length
-      val bar = if (w > 0) {
-        val percent = w * s.numCompletedTasks() / total
-        (0 until w).map { i =>
-          if (i < percent) "=" else if (i == percent) ">" else " "
+    // Schedule a refresh thread to run periodically
+    private val timer = new Timer("refresh progress", true)
+    timer.schedule(new TimerTask {
+        override def run() {
+            refresh()
+        }
+    }, firstDelayMSec, updatePeriodMSec)
+
+    /**
+      * Try to refresh the progress bar in every cycle
+      */
+    private def refresh(): Unit = synchronized {
+        val now = System.currentTimeMillis()
+        if (now - lastFinishTime < firstDelayMSec) {
+            return
+        }
+        val stageIds = sc.statusTracker.getActiveStageIds()
+        val stages = stageIds.flatMap(sc.statusTracker.getStageInfo).filter(_.numTasks() > 1)
+            .filter(now - _.submissionTime() > firstDelayMSec).sortBy(_.stageId())
+        if (stages.length > 0) {
+            show(now, stages.take(3)) // display at most 3 stages in same time
+        }
+    }
+
+    /**
+      * Show progress bar in console. The progress bar is displayed in the next line
+      * after your last output, keeps overwriting itself to hold in one line. The logging will follow
+      * the progress bar, then progress bar will be showed in next line without overwrite logs.
+      */
+    private def show(now: Long, stages: Seq[SparkStageInfo]) {
+        val width = TerminalWidth / stages.size
+        val bar = stages.map { s =>
+            val total = s.numTasks()
+            val header = s"[Stage ${s.stageId()}:"
+            val tailer = s"(${s.numCompletedTasks()} + ${s.numActiveTasks()}) / $total]"
+            val w = width - header.length - tailer.length
+            val bar = if (w > 0) {
+                val percent = w * s.numCompletedTasks() / total
+                (0 until w).map { i =>
+                    if (i < percent) "=" else if (i == percent) ">" else " "
+                }.mkString("")
+            } else {
+                ""
+            }
+            header + bar + tailer
         }.mkString("")
-      } else {
-        ""
-      }
-      header + bar + tailer
-    }.mkString("")
 
-    // only refresh if it's changed OR after 1 minute (or the ssh connection will be closed
-    // after idle some time)
-    if (bar != lastProgressBar || now - lastUpdateTime > 60 * 1000L) {
-      System.err.print(CR + bar)
-      lastUpdateTime = now
+        // only refresh if it's changed OR after 1 minute (or the ssh connection will be closed
+        // after idle some time)
+        if (bar != lastProgressBar || now - lastUpdateTime > 60 * 1000L) {
+            System.err.print(CR + bar)
+            lastUpdateTime = now
+        }
+        lastProgressBar = bar
     }
-    lastProgressBar = bar
-  }
 
-  /**
-   * Clear the progress bar if showed.
-   */
-  private def clear() {
-    if (!lastProgressBar.isEmpty) {
-      System.err.printf(CR + " " * TerminalWidth + CR)
-      lastProgressBar = ""
+    /**
+      * Clear the progress bar if showed.
+      */
+    private def clear() {
+        if (!lastProgressBar.isEmpty) {
+            System.err.printf(CR + " " * TerminalWidth + CR)
+            lastProgressBar = ""
+        }
     }
-  }
 
-  /**
-   * Mark all the stages as finished, clear the progress bar if showed, then the progress will not
-   * interweave with output of jobs.
-   */
-  def finishAll(): Unit = synchronized {
-    clear()
-    lastFinishTime = System.currentTimeMillis()
-  }
+    /**
+      * 将所有阶段标记为已完成，如果显示则清除进度条，则进度将不会与作业的输出交织在一起。
+      *
+      * Mark all the stages as finished, clear the progress bar if showed, then the progress will not
+      * interweave with output of jobs.
+      */
+    def finishAll(): Unit = synchronized {
+        clear()
+        lastFinishTime = System.currentTimeMillis()
+    }
 
-  /**
-   * Tear down the timer thread.  The timer thread is a GC root, and it retains the entire
-   * SparkContext if it's not terminated.
-   */
-  def stop(): Unit = timer.cancel()
+    /**
+      * 拆下计时器线程。计时器线程是GC根，如果未终止，它将保留整个SparkContext。
+      *
+      * Tear down the timer thread.  The timer thread is a GC root, and it retains the entire
+      * SparkContext if it's not terminated.
+      */
+    def stop(): Unit = timer.cancel()
 }
