c7565e4b7478d6bdb28c3bf776bdb2c0a995ae8e
baolibin <2578858653@qq.com>
2022-08-23 22:44:56 +0800

    update

--- SparkCore/src/main/java/org/apache/spark/unsafe/map/HashMapGrowthStrategy.java
+++ SparkCore/src/main/java/org/apache/spark/unsafe/map/HashMapGrowthStrategy.java
@@ -18,24 +18,28 @@
 package org.apache.spark.unsafe.map;
 
 /**
+ * 接口，该接口定义了当哈希映射超过阈值时如何增加哈希映射的大小。
+ * <p>
  * Interface that defines how we can grow the size of a hash map when it is over a threshold.
  */
 public interface HashMapGrowthStrategy {
 
-  int nextCapacity(int currentCapacity);
+    int nextCapacity(int currentCapacity);
 
-  /**
-   * Double the size of the hash map every time.
-   */
-  HashMapGrowthStrategy DOUBLING = new Doubling();
+    /**
+     * 每次将哈希映射的大小加倍。
+     * <p>
+     * Double the size of the hash map every time.
+     */
+    HashMapGrowthStrategy DOUBLING = new Doubling();
 
-  class Doubling implements HashMapGrowthStrategy {
-    @Override
-    public int nextCapacity(int currentCapacity) {
-      assert (currentCapacity > 0);
-      // Guard against overflow
-      return (currentCapacity * 2 > 0) ? (currentCapacity * 2) : Integer.MAX_VALUE;
+    class Doubling implements HashMapGrowthStrategy {
+        @Override
+        public int nextCapacity(int currentCapacity) {
+            assert (currentCapacity > 0);
+            // Guard against overflow
+            return (currentCapacity * 2 > 0) ? (currentCapacity * 2) : Integer.MAX_VALUE;
+        }
     }
-  }
 
 }
