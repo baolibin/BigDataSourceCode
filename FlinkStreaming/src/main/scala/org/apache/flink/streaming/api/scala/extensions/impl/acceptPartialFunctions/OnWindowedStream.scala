3466b721d425ed9ae8d6fc56cd463061beffcf01
baolibin <2578858653@qq.com>
2022-11-29 21:52:17 +0800

    update

--- FlinkStreaming/src/main/scala/org/apache/flink/streaming/api/scala/extensions/impl/acceptPartialFunctions/OnWindowedStream.scala
+++ FlinkStreaming/src/main/scala/org/apache/flink/streaming/api/scala/extensions/impl/acceptPartialFunctions/OnWindowedStream.scala
@@ -34,58 +34,60 @@ import org.apache.flink.util.Collector
   */
 class OnWindowedStream[T, K, W <: Window](stream: WindowedStream[T, K, W]) {
 
-  /**
-    * Applies a reduce function to the window. The window function is called for each evaluation
-    * of the window for each key individually. The output of the reduce function is interpreted
-    * as a regular non-windowed stream.
-    *
-    * This window will try and pre-aggregate data as much as the window policies permit.
-    * For example,tumbling time windows can perfectly pre-aggregate the data, meaning that only one
-    * element per key is stored. Sliding time windows will pre-aggregate on the granularity of the
-    * slide interval, so a few elements are stored per key (one per slide interval).
-    * Custom windows may not be able to pre-aggregate, or may need to store extra values in an
-    * aggregation tree.
-    *
-    * @param function The reduce function.
-    * @return The data stream that is the result of applying the reduce function to the window.
-    */
-  @PublicEvolving
-  def reduceWith(function: (T, T) => T) =
-    stream.reduce(function)
+    /**
+      * Applies a reduce function to the window. The window function is called for each evaluation
+      * of the window for each key individually. The output of the reduce function is interpreted
+      * as a regular non-windowed stream.
+      *
+      * This window will try and pre-aggregate data as much as the window policies permit.
+      * For example,tumbling time windows can perfectly pre-aggregate the data, meaning that only one
+      * element per key is stored. Sliding time windows will pre-aggregate on the granularity of the
+      * slide interval, so a few elements are stored per key (one per slide interval).
+      * Custom windows may not be able to pre-aggregate, or may need to store extra values in an
+      * aggregation tree.
+      *
+      * @param function The reduce function.
+      * @return The data stream that is the result of applying the reduce function to the window.
+      */
+    @PublicEvolving
+    def reduceWith(function: (T, T) => T) =
+        stream.reduce(function)
 
-  /**
-    * Applies the given fold function to each window. The window function is called for each
-    * evaluation of the window for each key individually. The output of the reduce function is
-    * interpreted as a regular non-windowed stream.
-    *
-    * @param function The fold function.
-    * @return The data stream that is the result of applying the fold function to the window.
-    */
-  @PublicEvolving
-  def foldWith[R: TypeInformation](initialValue: R)(function: (R, T) => R) =
-    stream.fold(initialValue)(function)
+    /**
+      * 将给定的折叠功能应用于每个窗口。每个键的每个窗口求值都会单独调用窗口函数。reduce函数的输出被解释为规则的非窗口流。
+      *
+      * Applies the given fold function to each window. The window function is called for each
+      * evaluation of the window for each key individually. The output of the reduce function is
+      * interpreted as a regular non-windowed stream.
+      *
+      * @param function The fold function.
+      * @return The data stream that is the result of applying the fold function to the window.
+      */
+    @PublicEvolving
+    def foldWith[R: TypeInformation](initialValue: R)(function: (R, T) => R) =
+        stream.fold(initialValue)(function)
 
-  /**
-    * Applies the given window function to each window. The window function is called for each
-    * evaluation of the window for each key individually. The output of the window function is
-    * interpreted as a regular non-windowed stream.
-    *
-    * Arriving data is incrementally aggregated using the given fold function.
-    *
-    * @param initialValue The initial value of the fold
-    * @param foldFunction The fold function that is used for incremental aggregation
-    * @param windowFunction The window function.
-    * @return The data stream that is the result of applying the window function to the window.
-    */
-  @PublicEvolving
-  def applyWith[ACC: TypeInformation, R: TypeInformation](
-      initialValue: ACC)(
-      foldFunction: (ACC, T) => ACC,
-      windowFunction: (K, W, Stream[ACC]) => TraversableOnce[R])
+    /**
+      * Applies the given window function to each window. The window function is called for each
+      * evaluation of the window for each key individually. The output of the window function is
+      * interpreted as a regular non-windowed stream.
+      *
+      * Arriving data is incrementally aggregated using the given fold function.
+      *
+      * @param initialValue   The initial value of the fold
+      * @param foldFunction   The fold function that is used for incremental aggregation
+      * @param windowFunction The window function.
+      * @return The data stream that is the result of applying the window function to the window.
+      */
+    @PublicEvolving
+    def applyWith[ACC: TypeInformation, R: TypeInformation](
+                                                               initialValue: ACC)(
+                                                               foldFunction: (ACC, T) => ACC,
+                                                               windowFunction: (K, W, Stream[ACC]) => TraversableOnce[R])
     : DataStream[R] =
-    stream.fold(initialValue, foldFunction, {
-      (key: K, window: W, items: Iterable[ACC], out: Collector[R]) =>
-        windowFunction(key, window, items.toStream).foreach(out.collect)
-    })
+        stream.fold(initialValue, foldFunction, {
+            (key: K, window: W, items: Iterable[ACC], out: Collector[R]) =>
+                windowFunction(key, window, items.toStream).foreach(out.collect)
+        })
 
 }
