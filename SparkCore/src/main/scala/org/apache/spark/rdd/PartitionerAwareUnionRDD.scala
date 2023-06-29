63c23a3993052adcc989f76e0d2bed8bbd1b432f
baolibin <2578858653@qq.com>
2023-06-29 23:08:40 +0800

    update

--- SparkCore/src/main/scala/org/apache/spark/rdd/PartitionerAwareUnionRDD.scala
+++ SparkCore/src/main/scala/org/apache/spark/rdd/PartitionerAwareUnionRDD.scala
@@ -25,6 +25,8 @@ import org.apache.spark.{OneToOneDependency, Partition, SparkContext, TaskContex
 import scala.reflect.ClassTag
 
 /**
+  * 类表示PartitionerAwareUnionRDD的分区，该类维护父RDD的相应分区的列表。
+  * 
   * Class representing partitions of PartitionerAwareUnionRDD, which maintains the list of
   * corresponding partitions of parent RDDs.
   */
