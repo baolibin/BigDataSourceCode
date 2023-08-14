4e4882e3f38f14bd611a5ce75bc72dc30970ef7b
baolibin <2578858653@qq.com>
2023-08-14 21:56:44 +0800

    update

--- SparkCore/src/main/scala/org/apache/spark/metrics/MetricsConfig.scala
+++ SparkCore/src/main/scala/org/apache/spark/metrics/MetricsConfig.scala
@@ -30,121 +30,123 @@ import org.apache.spark.util.Utils
 
 private[spark] class MetricsConfig(conf: SparkConf) extends Logging {
 
-  private val DEFAULT_PREFIX = "*"
-  private val INSTANCE_REGEX = "^(\\*|[a-zA-Z]+)\\.(.+)".r
-  private val DEFAULT_METRICS_CONF_FILENAME = "metrics.properties"
+    private val DEFAULT_PREFIX = "*"
+    private val INSTANCE_REGEX = "^(\\*|[a-zA-Z]+)\\.(.+)".r
+    private val DEFAULT_METRICS_CONF_FILENAME = "metrics.properties"
 
-  private[metrics] val properties = new Properties()
-  private[metrics] var perInstanceSubProperties: mutable.HashMap[String, Properties] = null
+    private[metrics] val properties = new Properties()
+    private[metrics] var perInstanceSubProperties: mutable.HashMap[String, Properties] = null
 
-  private def setDefaultProperties(prop: Properties) {
-    prop.setProperty("*.sink.servlet.class", "org.apache.org.apache.spark.metrics.sink.MetricsServlet")
-    prop.setProperty("*.sink.servlet.path", "/metrics/json")
-    prop.setProperty("master.sink.servlet.path", "/metrics/master/json")
-    prop.setProperty("applications.sink.servlet.path", "/metrics/applications/json")
-  }
-
-  /**
-   * Load properties from various places, based on precedence
-   * If the same property is set again latter on in the method, it overwrites the previous value
-   */
-  def initialize() {
-    // Add default properties in case there's no properties file
-    setDefaultProperties(properties)
-
-    loadPropertiesFromFile(conf.getOption("org.apache.spark.metrics.conf"))
-
-    // Also look for the properties in provided Spark configuration
-    val prefix = "org.apache.spark.metrics.conf."
-    conf.getAll.foreach {
-      case (k, v) if k.startsWith(prefix) =>
-        properties.setProperty(k.substring(prefix.length()), v)
-      case _ =>
+    private def setDefaultProperties(prop: Properties) {
+        prop.setProperty("*.sink.servlet.class", "org.apache.org.apache.spark.metrics.sink.MetricsServlet")
+        prop.setProperty("*.sink.servlet.path", "/metrics/json")
+        prop.setProperty("master.sink.servlet.path", "/metrics/master/json")
+        prop.setProperty("applications.sink.servlet.path", "/metrics/applications/json")
     }
 
-    // Now, let's populate a list of sub-properties per instance, instance being the prefix that
-    // appears before the first dot in the property name.
-    // Add to the sub-properties per instance, the default properties (those with prefix "*"), if
-    // they don't have that exact same sub-property already defined.
-    //
-    // For example, if properties has ("*.class"->"default_class", "*.path"->"default_path,
-    // "driver.path"->"driver_path"), for driver specific sub-properties, we'd like the output to be
-    // ("driver"->Map("path"->"driver_path", "class"->"default_class")
-    // Note how class got added to based on the default property, but path remained the same
-    // since "driver.path" already existed and took precedence over "*.path"
-    //
-    perInstanceSubProperties = subProperties(properties, INSTANCE_REGEX)
-    if (perInstanceSubProperties.contains(DEFAULT_PREFIX)) {
-      val defaultSubProperties = perInstanceSubProperties(DEFAULT_PREFIX).asScala
-      for ((instance, prop) <- perInstanceSubProperties if (instance != DEFAULT_PREFIX);
-           (k, v) <- defaultSubProperties if (prop.get(k) == null)) {
-        prop.put(k, v)
-      }
-    }
-  }
+    /**
+      * 根据优先级从不同位置加载属性，如果稍后在方法中再次设置相同的属性，则会覆盖以前的值
+      *
+      * Load properties from various places, based on precedence
+      * If the same property is set again latter on in the method, it overwrites the previous value
+      */
+    def initialize() {
+        // Add default properties in case there's no properties file
+        setDefaultProperties(properties)
 
-  /**
-   * Take a simple set of properties and a regex that the instance names (part before the first dot)
-   * have to conform to. And, return a map of the first order prefix (before the first dot) to the
-   * sub-properties under that prefix.
-   *
-   * For example, if the properties sent were Properties("*.sink.servlet.class"->"class1",
-   * "*.sink.servlet.path"->"path1"), the returned map would be
-   * Map("*" -> Properties("sink.servlet.class" -> "class1", "sink.servlet.path" -> "path1"))
-   * Note in the subProperties (value of the returned Map), only the suffixes are used as property
-   * keys.
-   * If, in the passed properties, there is only one property with a given prefix, it is still
-   * "unflattened". For example, if the input was Properties("*.sink.servlet.class" -> "class1"
-   * the returned Map would contain one key-value pair
-   * Map("*" -> Properties("sink.servlet.class" -> "class1"))
-   * Any passed in properties, not complying with the regex are ignored.
-   *
-   * @param prop the flat list of properties to "unflatten" based on prefixes
-   * @param regex the regex that the prefix has to comply with
-   * @return an unflatted map, mapping prefix with sub-properties under that prefix
-   */
-  def subProperties(prop: Properties, regex: Regex): mutable.HashMap[String, Properties] = {
-    val subProperties = new mutable.HashMap[String, Properties]
-    prop.asScala.foreach { kv =>
-      if (regex.findPrefixOf(kv._1.toString).isDefined) {
-        val regex(prefix, suffix) = kv._1.toString
-        subProperties.getOrElseUpdate(prefix, new Properties).setProperty(suffix, kv._2.toString)
-      }
-    }
-    subProperties
-  }
+        loadPropertiesFromFile(conf.getOption("org.apache.spark.metrics.conf"))
 
-  def getInstance(inst: String): Properties = {
-    perInstanceSubProperties.get(inst) match {
-      case Some(s) => s
-      case None => perInstanceSubProperties.getOrElse(DEFAULT_PREFIX, new Properties)
-    }
-  }
+        // Also look for the properties in provided Spark configuration
+        val prefix = "org.apache.spark.metrics.conf."
+        conf.getAll.foreach {
+            case (k, v) if k.startsWith(prefix) =>
+                properties.setProperty(k.substring(prefix.length()), v)
+            case _ =>
+        }
 
-  /**
-   * Loads configuration from a config file. If no config file is provided, try to get file
-   * in class path.
-   */
-  private[this] def loadPropertiesFromFile(path: Option[String]): Unit = {
-    var is: InputStream = null
-    try {
-      is = path match {
-        case Some(f) => new FileInputStream(f)
-        case None => Utils.getSparkClassLoader.getResourceAsStream(DEFAULT_METRICS_CONF_FILENAME)
-      }
-
-      if (is != null) {
-        properties.load(is)
-      }
-    } catch {
-      case e: Exception =>
-        val file = path.getOrElse(DEFAULT_METRICS_CONF_FILENAME)
-        logError(s"Error loading configuration file $file", e)
-    } finally {
-      if (is != null) {
-        is.close()
-      }
+        // Now, let's populate a list of sub-properties per instance, instance being the prefix that
+        // appears before the first dot in the property name.
+        // Add to the sub-properties per instance, the default properties (those with prefix "*"), if
+        // they don't have that exact same sub-property already defined.
+        //
+        // For example, if properties has ("*.class"->"default_class", "*.path"->"default_path,
+        // "driver.path"->"driver_path"), for driver specific sub-properties, we'd like the output to be
+        // ("driver"->Map("path"->"driver_path", "class"->"default_class")
+        // Note how class got added to based on the default property, but path remained the same
+        // since "driver.path" already existed and took precedence over "*.path"
+        //
+        perInstanceSubProperties = subProperties(properties, INSTANCE_REGEX)
+        if (perInstanceSubProperties.contains(DEFAULT_PREFIX)) {
+            val defaultSubProperties = perInstanceSubProperties(DEFAULT_PREFIX).asScala
+            for ((instance, prop) <- perInstanceSubProperties if (instance != DEFAULT_PREFIX);
+                 (k, v) <- defaultSubProperties if (prop.get(k) == null)) {
+                prop.put(k, v)
+            }
+        }
     }
-  }
+
+    /**
+      * Take a simple set of properties and a regex that the instance names (part before the first dot)
+      * have to conform to. And, return a map of the first order prefix (before the first dot) to the
+      * sub-properties under that prefix.
+      *
+      * For example, if the properties sent were Properties("*.sink.servlet.class"->"class1",
+      * "*.sink.servlet.path"->"path1"), the returned map would be
+      * Map("*" -> Properties("sink.servlet.class" -> "class1", "sink.servlet.path" -> "path1"))
+      * Note in the subProperties (value of the returned Map), only the suffixes are used as property
+      * keys.
+      * If, in the passed properties, there is only one property with a given prefix, it is still
+      * "unflattened". For example, if the input was Properties("*.sink.servlet.class" -> "class1"
+      * the returned Map would contain one key-value pair
+      * Map("*" -> Properties("sink.servlet.class" -> "class1"))
+      * Any passed in properties, not complying with the regex are ignored.
+      *
+      * @param prop  the flat list of properties to "unflatten" based on prefixes
+      * @param regex the regex that the prefix has to comply with
+      * @return an unflatted map, mapping prefix with sub-properties under that prefix
+      */
+    def subProperties(prop: Properties, regex: Regex): mutable.HashMap[String, Properties] = {
+        val subProperties = new mutable.HashMap[String, Properties]
+        prop.asScala.foreach { kv =>
+            if (regex.findPrefixOf(kv._1.toString).isDefined) {
+                val regex(prefix, suffix) = kv._1.toString
+                subProperties.getOrElseUpdate(prefix, new Properties).setProperty(suffix, kv._2.toString)
+            }
+        }
+        subProperties
+    }
+
+    def getInstance(inst: String): Properties = {
+        perInstanceSubProperties.get(inst) match {
+            case Some(s) => s
+            case None => perInstanceSubProperties.getOrElse(DEFAULT_PREFIX, new Properties)
+        }
+    }
+
+    /**
+      * Loads configuration from a config file. If no config file is provided, try to get file
+      * in class path.
+      */
+    private[this] def loadPropertiesFromFile(path: Option[String]): Unit = {
+        var is: InputStream = null
+        try {
+            is = path match {
+                case Some(f) => new FileInputStream(f)
+                case None => Utils.getSparkClassLoader.getResourceAsStream(DEFAULT_METRICS_CONF_FILENAME)
+            }
+
+            if (is != null) {
+                properties.load(is)
+            }
+        } catch {
+            case e: Exception =>
+                val file = path.getOrElse(DEFAULT_METRICS_CONF_FILENAME)
+                logError(s"Error loading configuration file $file", e)
+        } finally {
+            if (is != null) {
+                is.close()
+            }
+        }
+    }
 
 }
